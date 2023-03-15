b84e22cab5804660b4c55c2312867d43266f85e0
Bruno Sousa <brunoh.henriqueh01@gmail.com>
2023-03-15 17:17:17 -0300

    chore: :wrench: extends styles in tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,25 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'button': '#00AEFF'
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      backgroundImage: {
+        diablo: 'url(/assets/banner-hero/games/diablo-bg.png)',
+        hearthstone: 'url(/assets/banner-hero/games/hearthstone-bg.png)',
+        wow: 'url(/assets/banner-hero/games/wow-bg.png)',
+      },
+      screens: {
+        'sm': '375px',
+        'md': '768px',
+        'lg': '1440px',
+        'xl': '1920px'
+      }
+    },
   },
   plugins: [],
 }
